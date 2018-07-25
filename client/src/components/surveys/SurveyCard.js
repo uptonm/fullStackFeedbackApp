ba979e96510432b4899e18810d9685dceb20c315
Mike Upton <uptonm@wit.edu>
2018-07-25 15:13:10 -0400

    Chart tweaks, #TODO: Separate the chart object into a separate class.

--- client/src/components/surveys/SurveyCard.js
+++ client/src/components/surveys/SurveyCard.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import {BarChart, Bar} from 'recharts';
+import {BarChart, Bar, XAxis, YAxis, Tooltip, Legend} from 'recharts';
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: '11/12', yes: 3022, no: 2400},
+      {name: '11/13', yes: 2324, no: 1398},
+      {name: '11/14', yes: 3242, no: 3800}
 ];
 class SurveyCard extends Component {
   render() {
@@ -35,9 +31,15 @@ class SurveyCard extends Component {
               </p>
             </div>
           </div>
-          <div className='col s6'>
-            <BarChart height={200} width={425} data={data} style={{marginTop:'20px'}}>
-              <Bar type="monotone" dataKey="uv" barSize={30} fill="#8884d8"/>
+          <div className='col s6' style={{marginTop:'30px'}}>
+            <BarChart width={400} height={300} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+              <XAxis dataKey='name'/>
+              <YAxis />
+              <Tooltip />
+              <Bar type="monotone" dataKey="yes" barSize={30} fill="#26a69a"/>
+              <Bar type="monotone" dataKey="no" barSize={30} fill="#455a64"/>
+              <Legend />
             </BarChart>
           </div>
         </div>
